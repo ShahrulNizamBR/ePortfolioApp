ff158c0508881db2e347a80534e3ca8a950f9f6e
shahrulnizambr@gmail/com <shahrulnizambr@gmail.com>
2019-11-17 16:11:37 +0800

    Error codes

--- personal_portfolio/models.py
+++ personal_portfolio/models.py
@@ -6,7 +6,6 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
-    
 #BlogPost
 class Post(models.Model):
     title = models.CharField(max_length=255)
